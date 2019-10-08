5b1da397622aa5e290879e5bcaeb9bbf57a9e920
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-10-08 16:17:28 +0900

    Add -l

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func list(domain string) {
+func createEntries(domain string) {
 	resp, err := http.Get("https://ipv4.fetus.jp/" + domain + ".txt")
 	if err != nil {
 		log.Fatal(err)
@@ -29,8 +31,25 @@ func list(domain string) {
 }
 
 func main() {
+	list := flag.Bool("l", false, "list countries")
 	flag.Parse()
+
+	if *list {
+		doc, err := goquery.NewDocument("https://ipv4.fetus.jp/")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc.Find(".stupidtable tr").Each(func(i int, s *goquery.Selection) {
+			td := s.Find("td")
+			if td.Size() < 2 {
+				return
+			}
+			fmt.Printf("%s: %s\n", td.Eq(0).Text(), td.Eq(1).Text())
+		})
+		return
+	}
 	for _, arg := range flag.Args() {
-		list(arg)
+		createEntries(arg)
 	}
 }
